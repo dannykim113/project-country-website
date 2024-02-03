b492fa8c8c42a66750fed05c27700661bb5fd1ab
Danny Kim <dkim344@my.bcit.ca>
2024-02-03 13:55:15 -0800

    final draft

--- scripts/menu-toggle.js
+++ scripts/menu-toggle.js
@@ -1,16 +1,9 @@
-// Main Navigation Toggle
-const button = document.getElementById('main-nav-button');
-const links = document.getElementById('main-nav-list');
+const toggleBtn = document.getElementById('navbar-btn');
+const menu = document.getElementById('navbar-menu');
 
-button.addEventListener('click', function () {
-    links.classList.toggle('toggled');
-});
+console.log('check');
 
-
-// Thumb Navigation Toggle
-const thumbButton = document.getElementById('thumb-nav-button');
-const thumbLinks = document.getElementById('thumb-nav-secondary');
-
-thumbButton.addEventListener('click', function () {
-    thumbLinks.classList.toggle('toggled');
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    console.log('activated');
 });
